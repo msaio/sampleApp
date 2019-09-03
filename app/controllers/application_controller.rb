78111b1f0ca81bf93986cf8db5137403c5453aa6
msaio <msaiozero97@gmail.com>
2019-09-03 17:29:11 +0700

    Gotta reset WSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,8 @@ class ApplicationController < ActionController::Base
     <li>Play</li>
     <li>Talk</li>
     </ol>
+    <img src=\"https://media1.s-nbcnews.com/j/streams/2013/november/131106/8c9610617-codsalestease.nbcnews-ux-2880-1000.jpg\" width=\"50%\">
+    <img src=\"/assets/images/ghost.jpg\"><!-- for security this picture will not show up -->
     </html>".html_safe
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190126094327) do
-
-  create_table "games", force: :cascade do |t|
-    t.string "name"
-    t.string "pub"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "minigames", force: :cascade do |t|
-    t.integer "game_id"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180822004514) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
